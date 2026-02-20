4052aaf0d6758ed78f71794b6e931615364b2082
Ramandu777 <jonahpaulemch@gmail.com>
2026-02-20 16:01:56 +0800

    Add files via upload
    based.

---
+++ jumping.py
@@ -0,0 +1,101 @@
+import pygame
+
+win = pygame.display.set_mode((1100, 800))
+player_idle_image = pygame.transform.scale_by(
+    pygame.image.load('assets/Characters/red_knight.png').convert_alpha(), 2
+)
+jumping_player_image = pygame.transform.scale_by(
+    pygame.image.load('assets/Characters/jumping_red_knight.png').convert_alpha(), 2
+)
+punching_player_image = pygame.transform.scale_by(
+    pygame.image.load('assets/Characters/punching_red_knight.png').convert_alpha(), 2
+)
+
+velocity_x = 0
+velocity_y = 0
+move_speed = 20
+is_moving = False
+is_jumping = False
+facing = "right"
+jump_hold_duration = 0
+player_image = player_idle_image
+gravity = 1.2
+jump_initial_strength = 33
+player_x = 600
+player_y = 100
+CLOCK = pygame.time.Clock()
+    
+player_image_rect = player_image.get_rect(center=(player_x, player_y)) # draws the player at the x and y positions defined
+           
+
+running = True
+while running:
+
+
+    # Event Handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        
+        original_image = player_image_rect
+
+
+
+        # Jump start
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not is_jumping:
+                velocity_y = -jump_initial_strength
+                is_jumping = True
+        
+
+
+
+
+
+        # Jump release (cut jump short)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE and is_jumping and velocity_y < 0:
+                velocity_y *= 0.5
+    
+    keys = pygame.key.get_pressed()  
+    # sets velocity_x to zero every frame, unless a or d are pressed
+    velocity_x = 0
+    if keys[pygame.K_a]:
+        facing = "left"
+        velocity_x = - move_speed
+    elif keys[pygame.K_d]:
+        velocity_x = move_speed
+
+                       # Variable jump height while holding space (holy crap this took too long if your ever trying to add vriable jump, make sure you dont accidentaly redifine your y_velocity as some random variable)
+    if keys[pygame.K_SPACE] and is_jumping:  
+        velocity_y -= 0.2  # Extra lift while holding
+        player_image = jumping_player_image
+
+    # moves the player left or right based on velocity_x
+    player_image_rect.x += velocity_x
+        # Apply gravity
+    velocity_y += gravity
+    player_image_rect.y += velocity_y
+    
+                     # Flip the sprite image when facing left
+    if facing == "left":
+            player_image = pygame.transform.flip(player_image_rect, True, False)
+        
+    else:
+            player_image = player_image
+    if player_image_rect.bottom >= 700:
+        player_image_rect.bottom = 700
+        player_image = player_idle_image
+        velocity_y = 0
+        is_jumping = False
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:  # left click
+            # changes image
+            player_image = punching_player_image
+
+    pygame.display.update()
+    win.fill((40, 20, 0)) 
+    win.blit(player_image, player_image_rect)
+    CLOCK.tick(60) # framerate

9503d50677943da478506a2ca05f421c31567c46
Ramandu777 <jonahpaulemch@gmail.com>
2026-02-24 16:01:25 +0800

    Update jumping.py

--- jumping.py
+++ jumping.py
@@ -28,6 +28,8 @@ CLOCK = pygame.time.Clock()
 player_image_rect = player_image.get_rect(center=(player_x, player_y)) # draws the player at the x and y positions defined
            
 
+current_sprite = player_image
+
 running = True
 while running:
 
@@ -37,21 +39,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        
-        original_image = player_image_rect
-
-
-
         # Jump start
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and not is_jumping:
                 velocity_y = -jump_initial_strength
                 is_jumping = True
         
-
-
-
-
 
         # Jump release (cut jump short)
         if event.type == pygame.KEYUP:
@@ -65,35 +58,41 @@ while running:
         facing = "left"
         velocity_x = - move_speed
     elif keys[pygame.K_d]:
+        facing = "right"
         velocity_x = move_speed
 
                        # Variable jump height while holding space (holy crap this took too long if your ever trying to add vriable jump, make sure you dont accidentaly redifine your y_velocity as some random variable)
     if keys[pygame.K_SPACE] and is_jumping:  
         velocity_y -= 0.2  # Extra lift while holding
-        player_image = jumping_player_image
+        current_sprite = jumping_player_image
+        
 
     # moves the player left or right based on velocity_x
     player_image_rect.x += velocity_x
         # Apply gravity
     velocity_y += gravity
     player_image_rect.y += velocity_y
-    
-                     # Flip the sprite image when facing left
-    if facing == "left":
-            player_image = pygame.transform.flip(player_image_rect, True, False)
-        
-    else:
-            player_image = player_image
+
+
+            
     if player_image_rect.bottom >= 700:
         player_image_rect.bottom = 700
-        player_image = player_idle_image
+        current_sprite = player_idle_image
         velocity_y = 0
         is_jumping = False
 
     if event.type == pygame.MOUSEBUTTONDOWN:
         if event.button == 1:  # left click
             # changes image
-            player_image = punching_player_image
+            current_sprite = punching_player_image
+
+
+            # Flip the sprite image when facing left
+    if facing == "left":
+        player_image = pygame.transform.flip(current_sprite, True, False)
+    if facing == "right":
+        player_image = current_sprite
+    
 
     pygame.display.update()
     win.fill((40, 20, 0)) 
